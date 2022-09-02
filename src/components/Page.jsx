57df8559cd2bca7c4d354c7645aaa5a4afdbbeaf
Leogautier <leo.gautier3@gmail.com>
2022-09-02 10:15:54 +0200

    finis

---
+++ src/components/Page.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <>
+        <div>
+            <h1>
+            La boutique de Léo Gautier
+            </h1>
+        </div>
+        </>
+    );
+};
+
+export default Page;
