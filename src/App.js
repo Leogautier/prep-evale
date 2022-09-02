ec25189ec26639e98acfce37c30276d34de1c795
Leogautier <leo.gautier3@gmail.com>
2022-09-02 11:52:04 +0200

    finis

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import Page from './components/Page';
 import Card from "./components/Card";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 
 function App() {
+
+  const cardContenents = [
+
+    { id: "1", title: "test", image: "https://picsum.photos/id/237/200/300", description: "test tet" },
+    { id: "2", title: "test", image: "https://picsum.photos/id/236/200/300", description: "tet tet" },
+    { id: "3", title: "test", image: "https://picsum.photos/id/235/200/300", description: "tes tet" },
+    { id: "4", title: "test", image: "https://picsum.photos/id/234/200/300", description: "tt tet" }
+
+  ]
+
+
+  const productElements = cardContenents.map((categorie) =>(
+    <div className='col-12 col-lg-3' key={categorie.id}>
+      <Card title={categorie.title} image={categorie.image} description={categorie.description} />
+    </div>
+  ));
+
   return (
     <>
       <div>
         <Page />
-        <Card/>
+      </div>
+      <div className="row container">
+      {productElements}
       </div>
     </>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,16 @@
+
 import React from 'react';
 
-const Card = () => {
+const Card = ( {title, description, image}) => {
     return (
         <>
-            <div className="card w-25 container">
-                <img src="https://picsum.photos/id/237/200/300" className="card-img-top" />
-                <div className="card-body">
-                    <h5 className="card-title">test</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
+        
+            <div class="col" >
+                <img src={image} className="card-img-top"/>
+                    <div class="card-body">
+                        <h5 class="card-title">{title}</h5>
+                        <p class="card-text">{description}</p>
+                    </div>
             </div>
         </>
     );
