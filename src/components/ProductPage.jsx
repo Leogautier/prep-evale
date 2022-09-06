9e6a1c78b8277bd6709632a4b495c224e7f1f428
Leogautier <leo.gautier3@gmail.com>
2022-09-06 15:02:33 +0200

    finis

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 const ProductPage = () => {
+
+
+    useEffect(() => {
+
+        fetch("https://my.api.mockaroo.com/products.json?key=f4d18fd0")
+        .then((reponse) => reponse.json())
+        .then((data) => console.log(data));
+        
+    }, []);
+
     return (
         <div>
             <h1>
-            Product Screen
+                Product Screen
             </h1>
+
         </div>
     );
 };
